a38caec55abef68974406f42b5860d719b7d4cb5
Gandalf <gandy93@gmail.com>
2012-01-13 19:33:58 +0100

    Add some helper methods.

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function openLinksNewTab(selector) {
 		return false;
 	});
 }
+
+function clear() {
+	$('#content').empty();
+	$('#list').empty();
+}
+
+function api(url, success, error) {
+	var user = localStorage["username"];
+	var pass = localStorage["password"];
+
+	$.ajax({
+		url : url,
+		contentType: 'application/xml; charset=utf-8',
+		dataType : 'xml',
+		beforeSend : function(xhr) {
+			xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(user+':'+pass))));
+		},
+		success : success,
+		error : function(xml, msg, error) {
+			$('#content').empty().append('<p>An unexpected failure! '+msg+' - '+error+'.</p>');
+			$('#container').css({'min-width' : '300px'});
+		}
+	});
+}
+
+function say(str) {
+	$('#content').empty();
+	$('#content').empty().append('<p>'+str+'.</p>');
+}
